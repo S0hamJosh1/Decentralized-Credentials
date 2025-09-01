d23826af5779b948bcef33c40c57e1eeced615ae
SuperSoham007 <89575850+S0hamJosh1@users.noreply.github.com>
2025-09-02 00:15:54 +0530

    Delete deploy.ts

--- deploy.ts
+++
@@ -1,7 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-export default buildModule("Deploy", (m) => {
-  // Your contract class name from contracts/MinimalRegistry.sol
-  const main = m.contract("MinimalRegistry"); 
-  return { main };
-});
